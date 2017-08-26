d656b024ab3080083a83c6980d92b3dd975b2423
Jan <jan.atac@naturalinterface.fr>
2017-08-26 09:20:40 +0200

    Done

--- MakingAnagrams.playground/Contents.swift
+++ MakingAnagrams.playground/Contents.swift
@@ -1,14 +1,26 @@
 //: Playground - noun: a place where people can play
 
-var line1 = "abcdefgxxh"
-var line2 = "hgfedcaxxa"
+var line1 = "AbCdef"
+var line2 = "abcDEF"
 var counter = 0
+line1 = line1.lowercased()
+line2 = line2.lowercased()
 
 var line1Tmp = line1.characters.count >= line2.characters.count ? line1:line2
 var line2Tmp = line1.characters.count < line2.characters.count ? line1:line2
 var letterProcessed:[Character] = [Character]()
 
 line1Tmp.characters.forEach({element in
+ if letterProcessed.contains(element) {
+  return
+ }
+ letterProcessed.append(element)
+ let line1Tmp2 = line1Tmp.characters.filter({$0 == element})
+ let line2Tmp2 = line2Tmp.characters.filter({$0 == element})
+ counter = counter + (abs(line1Tmp2.count - line2Tmp2.count))
+})
+
+line2Tmp.characters.forEach({element in
  if letterProcessed.contains(element) {
   return
  }
